dbfb96e942e81096ac00346819241e0e8c555be0
Ricardo Barbosa <Bandito@Ricardos-MacBook-Pro-2.local>
2019-05-25 10:15:31 -0700

    Callbacks Finished

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -27,24 +27,32 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  cb(arr [arr.lnegth-1]);
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  z = x + y;
+  cb(z);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  z = x * y;
+  cb(z);
 }
 
-function contains(item, list, cb) {
+function contains(items, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  cb(list.includes(items))
 }
+contains('Pencil', items, console.log)
 
 /* STRETCH PROBLEM */
 
